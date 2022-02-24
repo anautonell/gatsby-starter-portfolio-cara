3c92ce1dd61173364a638c8a18a4779f5f8246a9
anautonell <91790208+anautonell@users.noreply.github.com>
2022-02-24 12:05:51 +0100

    Update html.js

--- html.js
+++ html.js
@@ -34,3 +34,27 @@ HTML.propTypes = {
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
 }
+
+<div
+    key={`loader`}
+              id="___loader"
+              style={{
+                alignItems: "center",
+                backgroundColor: "#F2F2F2",
+                display: "flex",
+                justifyContent: "center",
+                position: "absolute",
+                left: 0,
+                top: 0,
+                right: 0,
+                bottom: 0,
+                zIndex: 100,
+              }}
+           >
+              <img 
+                src={LoaderSVG} 
+                alt="loading spinner" 
+                width="150" 
+                height="150"
+              />
+        </div>
